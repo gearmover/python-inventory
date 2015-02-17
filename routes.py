dd4db88bf1226b9b4c59e77f0551d10a600eeb16
Chris Pergrossi <c.pergrossi@ufl.edu>
2015-02-17 05:54:31 -0500

    alot of work complete, next is update db and authentication

--- routes.py
+++ routes.py
@@ -1,99 +1,168 @@
-from flask import Flask, render_template, abort, redirect
+import flask
 import pymongo
 import os
+import time
 
-from flask.ext.triangle import Triangle
+from flask.ext.stormpath import (
+    StormpathError,
+    StormpathManager,
+    User,
+    login_required,
+    login_user,
+    logout_user,
+    user,
+)
 
-app = Flask(__name__)
-Triangle(app)
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'xU3kyh3mPKtVw1ya8Lywn07WCPT3q8xRoDHB90mtjqAnce0pJS'
+app.config['STORMPATH_API_KEY_FILE'] = '/home/chris/.stormpath/apiKey.properties'
+app.config['STORMPATH_APPLICATION'] = 'My Application'
+
+stormpath_manager = StormpathManager(app)
 
 client = pymongo.MongoClient('localhost', 27017)
 db = client.inventory
 items = db.items
 
+cache = []
+cache_expires = 0
+cache_time = 60                     # 60 second cache time
 
-@app.route('/assets/<type>/<path>')
-def assets(type, path):
+def update_cache(offset=0, limit=999):
+    global cache
+    global cache_expires
+    global cache_time
+
     try:
-        logfile = open('log.out', 'w')
-        print >> logfile, "%s/templates/%s/%s" % (os.path.dirname(os.path.realpath(__file__)), type, path)
-        logfile.close()
+        cur_time = int(time.time())
 
-        path = path.replace('..', '')
-        asset = open(os.path.dirname(os.path.realpath(__file__)) + '/templates/' + "%s/%s" % (str(type), str(path)))
+        if cur_time >= cache_expires:
+            all_items = items.find({})
+            cache = [i for i in all_items]
 
-        asset = asset.readlines()
+            cache_expires = cur_time + cache_time
 
-        return '\n'.join(asset)
+            flask.flash('Cache Updated', "info")
 
+            index=0
+            for item in cache:
+                if 'ImageUrl' not in cache[index]:
+                    cache[index]['ImageUrl'] = 'http://placehold.it/350x250'
+                if 'Status' not in cache[index]:
+                    cache[index]['Status'] = 'In'
+                if 'IntBarcode' not in cache[index]:
+                    cache[index]['IntBarcode'] = 1941491841
+
+                index+=1
+
+        boff = int(offset)
+        blim = int(limit)
+
+        offset = boff
+        limit = blim
+
+        view = cache[offset:offset+limit]
+
+        return view
+    except RuntimeError as e:
+        flask.flash('database error. please try again later. ' + e.message, "error")
+
+        return flask.redirect('/')
+
+    return None
+
+@app.route('/assets/<t>/<filename>')
+def assets(filename, t):
+
+    try:
+        if '..' in filename or filename[0] == '/':
+            flask.abort(401)
+
+        if '.js' in filename:
+            f = open(os.path.dirname(__file__) + '/static/js/' + filename, "r")
+            return flask.send_file(f,'text/javascript')
+        elif '.css' in filename:
+            f = open(os.path.dirname(__file__) + '/static/css/' + filename, "r")
+            return flask.send_file(f,'text/css')
+        else:
+            flask.abort(401)
     except IOError as e:
+        out = open('log.out','a')
+        out.write('[!] IOError: %s: "%s", "%s"\n' % (e.strerror, e.filename, e.filename2))
+        out.close()
 
-        abort(404)
+        flask.flash("File not found. Please try your request again later.", "error")
 
+        return flask.redirect('/')
 
 @app.route('/404')
 def notfound():
     return 'Oops!  Looks like we\'ve lost a website!'
 
+@app.route('/items/count')
+def item_count():
+    return str(items.count())
+
 @app.route('/items/p/<offset>/<limit>', methods=['GET'])
 @app.route('/items')
 def list_items(offset=0, limit=20):
-    all_items = items.find({}).limit(int(limit)).skip(int(offset))
 
-    headers = items.find_one({}).keys()
+    view = update_cache(offset, limit)
 
-    for f in headers:
-        if '_id' in f:
-            headers.remove(f)
-        elif 'OCO Decal' in f:
-            headers.remove(f)
-        elif 'alib' in f:
-            headers.remove(f)
+    summary_headers = ["Model", "Manufacturer", "Serial Number", "Location"]
 
-    itemList = []
-    index = 0
+    display = {'Model': 'model',
+               'Manufacturer': 'manufacturer',
+                'Serial Number': 'serial',
+               'Location': 'location'}
 
-    for i in all_items:
-        itemList.append({})
-        for k in i:
-            try:
-                field = k.replace('&','').replace('"','')
-                itemList[index][field] = i[k].replace('u&#39;','').replace('"','')
-            except:
-                pass
+    return flask.render_template('list_item.html', items=view, display=display, headers=summary_headers)
 
-        index = index+1
+@app.route('/items/p/<offset>/<limit>', methods=['POST'])
+def slice_list_items(offset=0, limit=20):
+    view = update_cache(offset, limit)
 
-    return render_template('list_item.html', items=itemList, headers=headers)
+    summary_headers = ["Model", "Manufacturer", "Serial Number", "Location"]
+
+    display = {'Model': 'model',
+               'Manufacturer': 'manufacturer',
+                'Serial Number': 'serial',
+               'Location': 'location'}
+
+    return flask.render_template('item_table.html', items=view, display=display, headers=summary_headers)
+
+@app.route('/details/<name>')
+def detailed_item(name):
+
+    view = update_cache()
+
+    for i in view:
+        if name in str(i['_id']):
+            return flask.render_template('item_header.html', fields=i)
+
+    return 'oops :( ' + name
 
 @app.route('/items/p/<offset>/<limit>/json')
 @app.route('/items/json')
 def list_items_json(offset=0, limit=20):
-    all_items = items.find({}).limit(int(limit)).skip(int(offset))
+    view = update_cache(offset, limit)
 
-    headers = items.find_one({}).keys()
+    summary_headers = ["Model", "Manufacturer", "Serial Number", "Location"]
 
-    for f in headers:
-        if '_id' in f:
-            headers.remove(f)
-        elif 'OCO Decal' in f:
-            headers.remove(f)
-        elif 'alib' in f:
-            headers.remove(f)
+    display = {'Model': 'model',
+               'Manufacturer': 'manufacturer',
+                'Serial Number': 'serial',
+               'Location': 'location'}
 
-    for i in all_items:
-        for k in i:
-            k = k.replace('&','').replace('"','')
-            i[k] = i[k].replace('&','').replace('"','')
-
-    json = {'headers': headers, 'items': [i for i in all_items] }
+    json = {'headers': summary_headers, 'display': display, 'items': view}
 
     return str(json)
 
 
 @app.route('/')
 def hello():
-    return redirect('/items')
+    return flask.redirect('/items')
 
 
 if __name__ == "__main__":
